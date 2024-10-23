847441876341109631d3cfcecebfa8e6be18087e
BlackWhite <2285697543@qq.com>
2024-10-23 21:44:11 +0800

    +

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,10 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
-    // '@nuxt/image',
+    '@nuxt/image',
     '@nuxt/ui',
     '@nuxthq/studio',
-    // 'nuxt-og-image'
+    'nuxt-og-image'
   ],
 
   hooks: {
@@ -54,6 +54,13 @@ export default defineNuxtConfig({
         braceStyle: '1tbs'
       }
     }
+  },
+
+  image: {
+    provider: 'aliyun',
+    aliyun: {
+      baseURL: "https://oss.gcw.wiki",
+    },
   },
 
   compatibilityDate: '2024-07-11'
