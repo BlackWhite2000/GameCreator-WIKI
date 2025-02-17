9e55f4fcf27fb1377645015bc21200feb409df51
BlackWhite <2285697543@qq.com>
2025-02-17 12:16:58 +0800

    bah

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -5,18 +5,19 @@ const { footer } = useAppConfig()
 <template>
   <UFooter>
     <template #left>
-      {{ footer.credits }}
+      <span>{{ footer.credits }}</span>
+      <NuxtLink to="https://beian.miit.gov.cn/" target="_blank"
+        class="mx-2 hover:text-[var(--ui-primary)] transition-all">
+        粤ICP备2025372185号-1
+      </NuxtLink>
     </template>
 
     <template #right>
       <UColorModeButton v-if="footer?.colorMode" />
 
       <template v-if="footer?.links">
-        <UButton
-          v-for="(link, index) of footer?.links"
-          :key="index"
-          v-bind="{ color: 'neutral', variant: 'ghost', ...link }"
-        />
+        <UButton v-for="(link, index) of footer?.links" :key="index"
+          v-bind="{ color: 'neutral', variant: 'ghost', ...link }" />
       </template>
     </template>
   </UFooter>
